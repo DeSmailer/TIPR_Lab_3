e138927f13e2588d112bcd0a6e1ec090c9b5013f
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2023-11-13 18:47:29 +0200

    update

---
+++ Assets/YSA VFX/Red Hollow/Scripts/HueControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueControl : MonoBehaviour
+{
+    [Range(0, 1)] public float hue = 0;
+
+    public HueValue[] hues;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < hues.Length; i++)
+        {
+            hues[i].hue = hue;
+        }
+    }
+
+    //void Update()
+    //{
+    //    for (int i = 0; i < hues.Length; i++)
+    //    {
+    //        hues[i].hue = hue;
+    //    }
+    //}
+}
