b8184b1c632f3022b3927b2a65f0217472ad99f1
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2023-11-15 10:23:46 +0200

    winlose

--- Assets/1. Scripts/WinLoseController.cs
+++ Assets/1. Scripts/WinLoseController.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLoseController : MonoBehaviour
 {
     [SerializeField] private StepManager _stepManager;
 
+    [SerializeField] private GameObject _winLoseWindow;
+
     [SerializeField] private GameObject _winGameObject;
     [SerializeField] private GameObject _loseGameObject;
+    [SerializeField] private GameObject _drawGameObject;
 
     [SerializeField] private HP _myHP;
     [SerializeField] private HP _enemyHP;
 
     public void Initialize()
     {
+        _winLoseWindow.SetActive(false);
         _stepManager.OnStepEnd += Check;
     }
 
@@ -33,17 +38,34 @@ public class WinLoseController : MonoBehaviour
 
     private void Win()
     {
+        _winLoseWindow.SetActive(true);
 
+        _winGameObject.SetActive(true);
+        _loseGameObject.SetActive(false);
+        _drawGameObject.SetActive(false);
     }
 
     private void Lose()
     {
+        _winLoseWindow.SetActive(true);
 
+        _winGameObject.SetActive(false);
+        _loseGameObject.SetActive(true);
+        _drawGameObject.SetActive(false);
     }
 
     private void Draw()
     {
+        _winLoseWindow.SetActive(true);
 
+        _winGameObject.SetActive(false);
+        _loseGameObject.SetActive(false);
+        _drawGameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     private void OnDestroy()
